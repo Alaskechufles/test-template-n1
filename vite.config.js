be888b4e5f5730f45706c1d60fbde39cc6c288b8
Diego <diegohuarsayacruz@gmail.com>
2026-05-05 07:34:46 -0500

    fix: update test page path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
-                test: resolve(__dirname, 'pages/test.html'),
+                test: resolve(__dirname, 'pages/pc1.html'),
             }
         }
     }
